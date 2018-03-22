c6aba68e7f0144391b8218346a1868599b5d4450
Jojo Tutor <josaphat@dnamicro.com>
2018-03-22 22:42:43 +0800

    added author page

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
       <Switch>
         <Route exact path='/' component={Home}/>
         <Route path='/about' component={About}/>
+        <Author path='/author' component={Author}/>
       </Switch>
     );
   }
