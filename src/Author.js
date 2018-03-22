55085031d143548ba9a1084230d3a98644a71686
Jojo Tutor <josaphat@dnamicro.com>
2018-03-22 23:00:50 +0800

    added styling

--- src/Author.js
+++ src/Author.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 
 import logo from './logo.svg';
 import './App.css';
@@ -10,7 +10,12 @@ class Author extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Author</h1>
+          <ul id="buttons">
+            <li><Link to={`/roster/`}>Author</Link></li>
+            <li><a href="#">ABOUT</a></li>
+            <li><a href="#">JOIN</a></li>
+            <li><a href="#">LOGIN</a></li>
+          </ul>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
