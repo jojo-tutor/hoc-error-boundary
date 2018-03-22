a01e7387359d16f120940b0917f1419d12b7b4e3
Jojo Tutor <josaphat@dnamicro.com>
2018-03-22 08:55:27 +0800

    added routes using react-router v4

---
+++ src/About.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import logo from './logo.svg';
+import './App.css';
+
+class About extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">About</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
+
+import Home from './Home';
+import About from './About';
+import Author from './Author';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route path='/about' component={About}/>
+      </Switch>
     );
   }
 }
