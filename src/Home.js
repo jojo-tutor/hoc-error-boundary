e28dce7c169a85152dfe7d606ecf15d2f3894983
Jojo Tutor <jeshrave003@gmail.com>
2018-04-30 12:42:15 +0800

    updated home container

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 import { withErrorHandler } from 'react-error-handler';
 import './App.css';
 
@@ -16,9 +17,9 @@ class Home extends Component {
         <h1 className="App-title">
           React Error Handler
         </h1>
-        <p>
-          Todo: [Documentation]
-        </p>
+        <p><Link to='/example1'>Example 1</Link></p>
+        <p><Link to='/example2'>Example 2</Link></p>
+        <p><Link to='/example2'>Example 3</Link></p>
       </div>
     );
   }
